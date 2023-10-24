67a0c77d112414c3354a1aea8c9d31e540ac54eb
ONUR BAHA KOÇ <koconurbaha@gmail.com>
2023-10-24 18:39:29 +0300

    C# Dersleri 37: Classlara Giriş

---
+++ CSharpCourse/Classes/Program.cs
@@ -0,0 +1,35 @@
+﻿// Classlara Giriş
+
+CustomerManager customerManager = new CustomerManager();
+
+customerManager.Add();
+customerManager.Update();
+
+ProductManager productManager = new ProductManager();
+
+productManager.Add();
+productManager.Update();
+
+class CustomerManager
+{
+    public void Add()
+    {
+        Console.WriteLine("Customer Added!");
+    }
+    public void Update()
+    {
+        Console.WriteLine("Customer Updated!");
+    }
+}
+
+class ProductManager
+{
+    public void Add()
+    {
+        Console.WriteLine("Product Added!");
+    }
+    public void Update()
+    {
+        Console.WriteLine("Product Updated!");
+    }
+}
