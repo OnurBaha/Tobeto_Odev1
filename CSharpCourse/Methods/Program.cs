3063e1fa9725c380a27e3c7d16de3a3808640761
ONUR BAHA KOÇ <koconurbaha@gmail.com>
2023-10-24 07:02:10 +0300

    C# Dersleri 25: Out Keyword İle Çalışmak

--- CSharpCourse/Methods/Program.cs
+++ CSharpCourse/Methods/Program.cs
@@ -16,6 +16,12 @@ var result3 = Add4(ref number1, number2);
 Console.WriteLine(result3);
 Console.WriteLine(number1);
 
+////Metot5, Out Keyword
+int number3;
+int number4 = 100;
+var result4 = Add5(out number3, number4);
+Console.WriteLine(result4);
+Console.WriteLine(number3);
 
 
 
@@ -45,4 +51,12 @@ static int Add4(ref int number1, int number2)
 {
     number1 = 30;
     return number1 + number2;
+}
+
+
+//Metot5, Out Keyword
+static int Add5(out int number3, int number4)
+{
+    number3 = 30;
+    return number3 + number4;
 }
