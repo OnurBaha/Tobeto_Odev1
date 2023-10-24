4a3ce6d16e22adea3f4c9c7ee56fb9f34157d340
ONUR BAHA KOÇ <koconurbaha@gmail.com>
2023-10-24 16:41:30 +0300

    C# Dersleri 28: Diziler

---
+++ CSharpCourse/Arrays/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] students = new string[3];
+students[0] = "Engin";
+students[1] = "Salih";
+students[2] = "Derin";
+
+string[] students2 = { "Ahmet", "Mehmet", "Ayşe" };
+
+foreach(var student in students)
+{
+    Console.WriteLine(student);
+}
+
+foreach(var student in students2)
+{
+    Console.WriteLine(student); 
+}
