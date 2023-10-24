c74afcb1791e2dc5368c311e90f14bdf3566bc1e
ONUR BAHA KOÇ <koconurbaha@gmail.com>
2023-10-24 16:56:18 +0300

    C# Dersleri 29: Çok Boyutlu Diziler

--- CSharpCourse/Arrays/Program.cs
+++ CSharpCourse/Arrays/Program.cs
@@ -1,4 +1,5 @@
-﻿string[] students = new string[3];
+﻿//Diziler
+string[] students = new string[3];
 students[0] = "Engin";
 students[1] = "Salih";
 students[2] = "Derin";
@@ -13,4 +14,21 @@ foreach(var student in students)
 foreach(var student in students2)
 {
     Console.WriteLine(student); 
+}
+
+
+// Çok Boyutlu Diziler
+string[,] regions = new string[5, 3]
+{
+    {"İstanbul", "İzmit", "Balıkesir"},
+    {"Ankara", "Konya", "Kırıkkale"},
+    {"Antalya", "Adana", "Mersin"},
+    {"Rize", "Trabzon", "Samsun"},
+    {"Muğla", "İzmir", "Manisa"},
+};
+
+for(int i = 0; i <= regions.GetUpperBound(0); i++)
+{
+    for (int j = 0; j <= regions.GetUpperBound(1); j++)
+        Console.WriteLine(regions[i, j]);
 }
