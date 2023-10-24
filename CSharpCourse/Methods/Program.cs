4fa859c33a0d5a7ddf634906e8a337f6d11361b1
ONUR BAHA KOÇ <koconurbaha@gmail.com>
2023-10-24 06:56:15 +0300

    C# Dersleri 24: Challenge : Ref Keyword İle Çalışmak

--- CSharpCourse/Methods/Program.cs
+++ CSharpCourse/Methods/Program.cs
@@ -9,6 +9,13 @@ Console.WriteLine(result);
 var result2 = Add3();
 Console.WriteLine(result2);
 
+//Metot4, Ref Keyword
+int number1 = 20;
+int number2 = 100;
+var result3 = Add4(ref number1, number2);
+Console.WriteLine(result3);
+Console.WriteLine(number1);
+
 
 
 
@@ -31,4 +38,11 @@ static int Add3(int number1=10, int number2=30)
 {
     var result2 = number1 + number2;
     return result2;
+}
+
+//Metot4, Ref Keyword
+static int Add4(ref int number1, int number2)
+{
+    number1 = 30;
+    return number1 + number2;
 }
