54188f44d6d234b095fb1cc185c61b4b90f77023
ONUR BAHA KOÇ <koconurbaha@gmail.com>
2023-10-24 18:02:00 +0300

    C# Dersleri 34: Örnek Uygulama : Asal Sayı

--- CSharpCourse/Loops/Program.cs
+++ CSharpCourse/Loops/Program.cs
@@ -1,5 +1,5 @@
 ﻿// For Döngüsü
-for(int i = 100; i>=0; i=i-2)
+for (int i = 100; i >= 0; i = i - 2)
 {
     Console.WriteLine(i);
 }
@@ -8,7 +8,7 @@ Console.WriteLine("Finished");
 
 // While Döngüsü
 int number = 100;
-while(number>=0)
+while (number >= 0)
 {
     Console.WriteLine(number);
     number--;
@@ -22,12 +22,44 @@ do
 {
     Console.WriteLine(number2);
     number2--;
-}while(number2>=11);
+} while (number2 >= 11);
 
 
 // ForEach Döngüsü
 string[] students = new string[3] { "Engin", "Derin", "Salih" };
-foreach(var student in students)
+foreach (var student in students)
 {
     Console.WriteLine(student);
 }
+
+
+
+//IsPrimeNumber Example
+if (IsPrimeNumber(6))
+{
+    Console.WriteLine("This is a prime number.");
+}
+else
+{
+    Console.WriteLine("This is a not prime number.");
+}
+
+static bool IsPrimeNumber(int number)
+{
+    bool result = true;
+    for (int i = 2; i < number - 1; i++)
+    {
+        if (number % i == 0)
+        {
+            result = false;
+            i = number;
+        }
+    }
+    return result;
+}
+
+
+
+
+
+
