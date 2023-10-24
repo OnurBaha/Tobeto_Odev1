f7a5b67996c5f9f799df35fce92ec35038284182
ONUR BAHA KOÇ <koconurbaha@gmail.com>
2023-10-24 05:39:58 +0300

    C# Dersleri 17: Else If Bloğuyla Çalışmak

--- CSharpCourse/Conditionals/Program.cs
+++ CSharpCourse/Conditionals/Program.cs
@@ -1,4 +1,4 @@
-﻿// if - else Bloğu
+﻿// if Bloğu
 var number = 11;
 
 if(number == 10)
@@ -11,8 +11,26 @@ else
 }
 
 
-
 // Single Line
 var number2 = 10;
 
 Console.WriteLine(number2 == 10 ? "Number2 is 10" : "Number2 is not 10");
+
+
+
+// else - if Bloğu
+var number3 = 30;
+
+if (number3 == 10)
+{
+    Console.WriteLine("Number3 is 10");
+}
+else if(number3 == 20)
+{
+    Console.WriteLine("Number3 is 20");
+}
+else
+{
+    Console.WriteLine("Number3 is not 10 or 20");
+}
+
