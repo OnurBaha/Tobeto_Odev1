432a1225947e198c42ade394a6ab8b3e0ae03b2d
ONUR BAHA KOÇ <koconurbaha@gmail.com>
2023-10-24 05:01:49 +0300

    C# Dersleri 7: Short Veri Tipi

--- CSharpCourse/TypesAndVariables/Program.cs
+++ CSharpCourse/TypesAndVariables/Program.cs
@@ -2,5 +2,7 @@
 //Value Types
 int number1 = 2147483647;
 long number2 = 9223372036854775807;
+short number3 = 32767;
 Console.WriteLine("Number is {0}", number1);
 Console.WriteLine("Number is {0}", number2);
+Console.WriteLine("Number is {0}", number3);
