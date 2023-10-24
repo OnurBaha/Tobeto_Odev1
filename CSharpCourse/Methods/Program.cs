9dbfed69f787c4a3cbd903bea5d6523c3cc0ad3e
ONUR BAHA KOÇ <koconurbaha@gmail.com>
2023-10-24 06:34:02 +0300

    C# Dersleri 22: Parametreli Metotlarla Çalışmak

--- CSharpCourse/Methods/Program.cs
+++ CSharpCourse/Methods/Program.cs
@@ -1,6 +1,22 @@
-﻿Add();
+﻿//Metot1
+Add();
 
+//Metot2
+var result = Add2(20, 30);
+Console.WriteLine(result);
+
+
+
+
+// Metot1
 static void Add()
 {
     Console.WriteLine("Added!");
+}
+
+//Metot2
+static int Add2(int number1, int number2)
+{
+    var result = number1 + number2;
+    return result;
 }
