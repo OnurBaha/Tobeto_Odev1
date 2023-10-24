550890de99114bcec042f1faf8970a2fbb505725
ONUR BAHA KOÇ <koconurbaha@gmail.com>
2023-10-24 17:15:55 +0300

    C# Dersleri 31: While Döngüsü

--- CSharpCourse/Loops/Program.cs
+++ CSharpCourse/Loops/Program.cs
@@ -1,6 +1,16 @@
-﻿// for Döngüsü
+﻿// For Döngüsü
 for(int i = 100; i>=0; i=i-2)
 {
     Console.WriteLine(i);
 }
 Console.WriteLine("Finished");
+
+
+// While Döngüsü
+int number = 100;
+while(number>=0)
+{
+    Console.WriteLine(number);
+    number--;
+}
+Console.WriteLine("Now number is {0}", number);
