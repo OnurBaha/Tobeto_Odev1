371b0e961e266dd5d86125de16a9db45607c8e8a
ONUR BAHA KOÇ <koconurbaha@gmail.com>
2023-10-24 04:59:28 +0300

    C# Dersleri 6: Long Veri Tipi

--- CSharpCourse/TypesAndVariables/Program.cs
+++ CSharpCourse/TypesAndVariables/Program.cs
@@ -1,4 +1,6 @@
 ﻿Console.WriteLine("Hello, World!");
 //Value Types
 int number1 = 2147483647;
+long number2 = 9223372036854775807;
 Console.WriteLine("Number is {0}", number1);
+Console.WriteLine("Number is {0}", number2);
