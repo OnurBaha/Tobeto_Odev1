feee083e261f60bfb540d25ba562c678cab259ba
ONUR BAHA KOÇ <koconurbaha@gmail.com>
2023-10-24 06:48:19 +0300

    C# Dersleri 23: Default Parametreli Metotlarla Çalışmak

--- CSharpCourse/Methods/Program.cs
+++ CSharpCourse/Methods/Program.cs
@@ -1,9 +1,14 @@
 ﻿//Metot1
 Add();
 
-//Metot2
+//Metot2, Parametreli Metot
 var result = Add2(20, 30);
 Console.WriteLine(result);
+
+//Metot3, Default Parametreli Metot
+var result2 = Add3();
+Console.WriteLine(result2);
+
 
 
 
@@ -14,9 +19,16 @@ static void Add()
     Console.WriteLine("Added!");
 }
 
-//Metot2
+//Metot2, Parametreli Metot
 static int Add2(int number1, int number2)
 {
     var result = number1 + number2;
     return result;
+}
+
+//Metot3, Default Parametreli Metot
+static int Add3(int number1=10, int number2=30)
+{
+    var result2 = number1 + number2;
+    return result2;
 }
