223507f8c4aecd203e8293db38397bc3099b70e3
ONUR BAHA KOÇ <koconurbaha@gmail.com>
2023-10-26 01:32:49 +0300

    C# Dersleri 46: Kalıtıma Giriş

---
+++ CSharpCourse/Inheritance/Program.cs
@@ -0,0 +1,36 @@
+﻿Person[] persons = new Person[3]
+{
+    new Customer
+    {
+        FirstName = "Onur"
+    },
+    new Student
+    {
+        FirstName = "Engin"
+    },
+    new Person
+    {
+        FirstName = "Salih"
+    }
+};
+
+foreach(var person in persons)
+{
+    Console.WriteLine(person.FirstName);
+}
+
+
+class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+class Customer : Person
+{
+    public string City { get; set; }
+}
+class Student : Person 
+{
+    public string Departmant { get; set; }
+}
