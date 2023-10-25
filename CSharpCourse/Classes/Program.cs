f78d1498e91bb3c7db7de6cd84a7e8c1df780f2b
ONUR BAHA KOÇ <koconurbaha@gmail.com>
2023-10-25 21:03:14 +0300

    C# Dersleri 38: Classlar İçinde Property Tanımlamak

--- CSharpCourse/Classes/Program.cs
+++ CSharpCourse/Classes/Program.cs
@@ -1,5 +1,27 @@
 ﻿// Classlara Giriş
 
+using Classes;
+
+// Classlar İçinde Property Tanımlamak
+Customer customer = new Customer();
+
+customer.City = "Ankara";
+customer.Id = 1;    
+customer.FirstName = "Onur";
+customer.LastName = "Baha";
+
+Customer customer2 = new Customer
+{
+    Id = 2,
+    City = "İstanbul",
+    FirstName = "Onur",
+    LastName = "Baha",
+};
+
+Console.WriteLine(customer2.FirstName);
+
+
+
 CustomerManager customerManager = new CustomerManager();
 
 customerManager.Add();
@@ -33,3 +55,4 @@ class ProductManager
         Console.WriteLine("Product Updated!");
     }
 }
+
