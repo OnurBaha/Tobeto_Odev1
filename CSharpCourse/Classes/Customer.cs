c79d56416b4be758f135aad3c2bb51642de50574
ONUR BAHA KOÇ <koconurbaha@gmail.com>
2023-10-25 21:17:13 +0300

    C# Dersleri 39: Encapsulation

--- CSharpCourse/Classes/Customer.cs
+++ CSharpCourse/Classes/Customer.cs
@@ -8,8 +8,23 @@ namespace Classes
 {
     internal class Customer
     {
+
+        //field
+        private string _firstName;
+
+        // Property
         public int Id { get; set; }
-        public string FirstName { get; set; }
+        public string FirstName 
+        {
+            get 
+            {
+                return "Mr." + _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            } 
+        }
         public string LastName { get; set; }
         public string City { get; set; }
     }
