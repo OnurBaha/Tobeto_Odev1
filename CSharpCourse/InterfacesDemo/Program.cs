3a800e6dc7f648ecb8943e53b0d6aab853a77faf
ONUR BAHA KOÇ <koconurbaha@gmail.com>
2023-10-26 01:20:16 +0300

    C# Dersleri 45: Çoklu İmplementasyon

---
+++ CSharpCourse/InterfacesDemo/Program.cs
@@ -0,0 +1,76 @@
+﻿IWorker[] workers = new IWorker[3]
+{
+    new Manager(),
+    new Worker(),
+    new Robot()
+};
+
+foreach (var worker in workers )
+{
+    worker.Work();
+}
+
+IEat[] eats = new IEat[2]
+{
+    new Manager(),
+    new Worker()
+};
+
+foreach(var eat in eats )
+{
+    eat.Eat();
+}
+
+
+interface IWorker
+{
+    void Work();
+}
+interface IEat
+{
+    void Eat();
+}
+interface ISalary
+{
+    void GetSalary();
+}
+
+
+class Manager : IWorker, ISalary, IEat
+{
+    public void Work()
+    {
+
+    }
+    public void Eat()
+    {
+
+    }
+    public void GetSalary()
+    {
+
+    }
+}
+class Worker : IWorker, ISalary, IEat
+{
+    public void Work()
+    {
+
+    }
+    public void Eat()
+    {
+
+    }
+    public void GetSalary()
+    {
+
+    }
+
+}
+class Robot : IWorker
+{
+    public void Work()
+    {
+
+    }
+}
