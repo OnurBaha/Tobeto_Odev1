4fb488aa46f17f857272a372f09ba9ea6771dd61
ONUR BAHA KOÇ <koconurbaha@gmail.com>
2023-10-25 23:13:08 +0300

    C# Dersleri 42: İnterfaceleri Neden Kullanıyoruz?

--- CSharpCourse/Interfaces/Program.cs
+++ CSharpCourse/Interfaces/Program.cs
@@ -1,15 +1,40 @@
-﻿interface IPerson
+﻿PersonManager manager = new PersonManager();
+Customer customer = new Customer
+{
+    Id = 1,
+    FirstName = "Onur",
+    LastName = "Baha",
+    Address = "İstanbul",
+};
+
+Student student = new Student
+{
+    Id = 2,
+    FirstName = "Engin",
+    LastName = "Demiroğ",
+    Departmant = "Computer Sciences",
+};
+
+manager.Add(customer);
+manager.Add(student);
+
+
+
+
+
+
+interface IPerson
 {
     int Id { get; set; }
-    int FirstName { get; set; }
-    int LastName { get; set; }
+    string FirstName { get; set; }
+    string LastName { get; set; }
 }
 
 class Customer : IPerson 
 {
     public int Id { get; set; }
-    public int FirstName { get; set; }
-    public int LastName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
 
     public string Address { get; set; }
 }
@@ -17,8 +42,25 @@ class Customer : IPerson
 class Student : IPerson
 {
     public int Id { get; set; }
-    public int FirstName { get; set; }
-    public int LastName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
 
     public string Departmant { get; set; }
+}
+
+class Worker : IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public string Departmant { get; set; }
+}
+
+class PersonManager
+{ 
+    public void Add(IPerson person)
+    {
+        Console.WriteLine(person.FirstName);
+    }
 }
