272104a60f6273790d579d12a6a2498e1cd356a6
ONUR BAHA KOÇ <koconurbaha@gmail.com>
2023-10-25 22:48:14 +0300

    C# Dersleri 41: İnterfacelere Temel Giriş

---
+++ CSharpCourse/Interfaces/Program.cs
@@ -0,0 +1,24 @@
+﻿interface IPerson
+{
+    int Id { get; set; }
+    int FirstName { get; set; }
+    int LastName { get; set; }
+}
+
+class Customer : IPerson 
+{
+    public int Id { get; set; }
+    public int FirstName { get; set; }
+    public int LastName { get; set; }
+
+    public string Address { get; set; }
+}
+
+class Student : IPerson
+{
+    public int Id { get; set; }
+    public int FirstName { get; set; }
+    public int LastName { get; set; }
+
+    public string Departmant { get; set; }
+}
